8de886932bb2dccc0597cdf4fa7eccc7b4817f69
iswindle <isaac.swindle@gmail.com>
2021-09-09 16:57:30 -0500

    Added LinkedList

---
+++ src/com/techex/datastructures/LinkedList.java
@@ -0,0 +1,13 @@
+package com.techex.datastructures;
+
+public class LinkedList {
+    Node head; // head of the list
+
+    /* Linked list Node*/
+    class Node {
+        int data;
+        Node next;
+
+        Node(int d) { data = d; }
+    }
+}
