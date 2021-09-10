25de0a6e4e752e37bb3d58d5090137cf3e395d79
iswindle <isaac.swindle@gmail.com>
2021-09-09 20:13:30 -0500

    Added LinearSearch

--- src/com/techex/Main.java
+++ src/com/techex/Main.java
@@ -1,11 +1,23 @@
 package com.techex;
 
 import com.techex.algorithms.sort.Bubble;
+import com.techex.algorithms.search.Linear;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Bubble.Sort(new int[]{5,4,2,1,9,6});
+        // Check for 0 (0)
+        System.out.println(Linear.search(new int[]{}, 5));
+
+        // Check for 1 (1)
+        System.out.println(Linear.search(new int[]{5,4,2,1,9,6}, 6));
+
+        // Check for Multiple (3)
+        System.out.println(Linear.search(new int[]{5,4,2,1,9,6,5,5}, 5));
+
+        // Check for cases that do not exist
+        System.out.println(Linear.search(new int[]{5,4,2,1,9,6,5,5}, 22));
+
     }
 }
