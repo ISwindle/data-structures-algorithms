e0e71cc57540ec60659a06df00c7d23c25ce91c8
iswindle <isaac.swindle@gmail.com>
2021-09-13 19:26:16 -0500

    Added NaiveSearch

--- src/com/techex/Main.java
+++ src/com/techex/Main.java
@@ -1,23 +1,13 @@
 package com.techex;
 
 import com.techex.algorithms.sort.Bubble;
-import com.techex.algorithms.search.Linear;
+import com.techex.algorithms.patterns.NaiveSearch;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // Check for 0 (0)
-        System.out.println(Linear.search(new int[]{}, 5));
-
-        // Check for 1 (1)
-        System.out.println(Linear.search(new int[]{5,4,2,1,9,6}, 6));
-
-        // Check for Multiple (3)
-        System.out.println(Linear.search(new int[]{5,4,2,1,9,6,5,5}, 5));
-
-        // Check for cases that do not exist
-        System.out.println(Linear.search(new int[]{5,4,2,1,9,6,5,5}, 22));
+        NaiveSearch.SearchText("TSU's football coach is Eddie George", "Eddie");
 
     }
 }
