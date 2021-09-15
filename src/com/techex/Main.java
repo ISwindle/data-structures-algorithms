890007ab8f65210b78f2d45a18ec04d9aa150c31
iswindle <isaac.swindle@gmail.com>
2021-09-14 19:24:03 -0500

    Changed Darth Vader Image and Location

--- src/com/techex/Main.java
+++ src/com/techex/Main.java
@@ -4,16 +4,19 @@ import com.techex.projects.audio.AudioPlayer;
 import com.techex.projects.images.DisplayImage;
 
 import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        DisplayImage displayImage = new DisplayImage("src/com/techex/resources/darth-vader-2.jpg");
+        Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
+        DisplayImage displayImage = new DisplayImage("src/com/techex/resources/star-wars-darth-vader.jpeg");
         JFrame frame = new JFrame();
         frame.add(displayImage);
         frame.setSize(400,400);
+        frame.setLocation(size.width/3, size.height/3);
         frame.setVisible(true);
         String audioFilePath = "src/com/techex/resources/ImperialMarch60.wav";
         AudioPlayer player = new AudioPlayer();
